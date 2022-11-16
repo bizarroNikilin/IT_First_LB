78c20b3c8a42be9250269cfbf45bcbb84e75d239
bizarroNikilin <andrii.for.projects@gmail.com>
2022-11-16 16:29:21 +0200

    2

---
+++ untitled/src/warehouse/Main.java
@@ -0,0 +1,16 @@
+package warehouse;
+import java.io.File;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.xml.sax.SAXException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
